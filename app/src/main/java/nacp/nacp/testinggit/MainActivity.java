58dce9598a5e512cb88e98279c861ac969342cbf
ravi <elbizsystemspvtltd@gmail.com>
2018-03-20 12:05:01 +0530

    ok

--- app/src/main/java/nacp/nacp/testinggit/MainActivity.java
+++ app/src/main/java/nacp/nacp/testinggit/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ActionBar ab = getSupportActionBar();
-        ab.hide();  
+        ab.hide();
+        // text view place
     }
 }
